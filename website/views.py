791b361c02c770cac7946003e3e17353ce77d437
lysne <lysne@live.no>
2016-04-24 17:02:22 +0200

    videreføring av kommunikasjon mellom pi og server

--- website/views.py
+++ website/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 import json
-
+from models import CoffeeBrewer
 
 
 
 
 # Create your views here.
 def brewer_post(request):
-	#unico = request.body.decode('utf-8')
-	#data = json.loads(unico)
 	if request.method == 'POST':
-		return
+		add_brewer(request.data)
 	elif request.method == 'GET':
-		print "body" + request.body
-		#get query from the request:
+		#Fetching RFID from request
 		s = request.META['QUERY_STRING']
-		
-		return HttpResponse("mjau")
+		return checkRFID(s)
 
 	#return HttpResponse("mjau")
-		
+
+def checkRFID(ID):
+	entry = CoffeeBrewer.objects.filter(RFID = ID)
+	response = HttpResponse()
+	if entry.exists():
+		response.content = True
+	else:
+		response.content = False
+	return response
+
+def add_brewer(data):
+	brewer = CoffeeBrewer(datetime = datetime.now(), RFID = data['RFID'], name = data['name'])
+	brewer.save()
