8d9e9c366b49720c3fa84792a6f9f7b3c8842134
Your Name <you@example.com>
2016-04-27 02:12:20 +0200

    I added sec_key to ignore

--- abakaffe/settings.py
+++ abakaffe/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+from secrets import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mlhuen6_79d@1cr4zjm_zg_x)hdm7c8n3_(b)t6bj$u@^5&dxt'
+# whoops
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
