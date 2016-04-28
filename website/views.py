b04165354ab9194edd739289ca2b041a334a38a6
Magnus Berge <magnusberge.mb@gmail.com>
2016-04-28 16:08:27 +0200

    Working with integrating the highscore-app. Not yet finished

--- website/views.py
+++ website/views.py
@@ -3,13 +3,15 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from update.models import Weight
+from highscores.views import view
 
 def index(request, template = "website/index.html"):
     context = {'WEIGHT' : Weight.objects.get(pk=1).weight}
     return render(request, template, context)
 
-def highscore(request):
-    return render(request, "website/highscore.html")
+def highscore(request, template = "website/highscore.html"):
+    context = view(request)
+    return render(request, template, context)
 
-def about(request):
-    return render(request, "website/about.html")
+def about(request, template = "website/about.html"):
+    return render(request, template)
