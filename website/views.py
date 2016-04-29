4282366ecbe7f7c9f8ef176e686739358755e835
alex <alex.h.bakken@gmail.com>
2016-04-30 01:30:13 +0200

    lwl

--- website/views.py
+++ website/views.py
@@ -5,6 +5,11 @@ from .models import Subscribe
 from datetime import datetime
 from .forms import NameForm
 import smtplib
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+import os
 
 
 def index(request, template="website/index.html"):
@@ -82,3 +87,66 @@ def subscribe(request):
     return render(request, "website/index.html", context)
 
     # forms.py => views => models.py => db.sqlite3
+
+
+### EMAIL SECTION HERE ####
+
+
+gmail_user = "abakaffenotifier@gmail.com"
+gmail_pwd = "nynoregpassord1337"
+
+
+def mail(to, attach=None):
+    # static things
+    subject = "Abakaffe Subcribe :)"
+
+    # text = "Thank you for subscribing to Abakaffe =)\n\n You will be notified when fresh good coffe is ready. \n\nBest regards, \n"
+    html = """\
+            <html>
+            <head></head>
+            <body>
+            <p>Hi!<br>
+            Thank you for subscribing to Abakaffe =)<br>
+            You will be notified when fresh good coffe is ready.
+            Here is the <a href="http://abakaffe.today">link</a> you wanted.
+            </p>
+            <p>Best regards, <br> </p>
+            <br>
+            <p>
+                <img src="https://49.media.tumblr.com/23b345acdf7f9639bf94bb9bbe8f8191/tumblr_nvu767z9eS1s30ko5o1_500.gif" alt="I need Coffee" height="150" width="250"/>
+
+            <br>
+            </p>
+            </body>
+            </html>
+         """
+
+    #
+    msg = MIMEMultipart()
+    msg['From'] = gmail_user
+    msg['To'] = to
+    msg['Subject'] = subject
+
+    # part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # msg.attach(part1)
+    msg.attach(part2)
+    try:
+        if attach:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(open(attach, 'rb').read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attach))
+            msg.attach(part)
+        mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmail_user, gmail_pwd)
+        mailServer.sendmail(gmail_user, to, msg.as_string())
+        mailServer.close()
+    except smtplib.SMTPConnectError as conn:
+        print("An email connection error: ", conn)
+    except smtplib.SMTPAuthenticationError as auth:
+        print("An email auth error: ", auth)
